6726eb62ab4b54641a69612a2dd2fe937bc289ea
Berwyn-s <berwyn.surya007@gmail.com>
2025-01-23 23:05:33 +0700

    Added section (why forest matters), not yet completed

--- js/script.js
+++ js/script.js
@@ -17,14 +17,12 @@ const myNav = document.getElementById('nav')
 window.onscroll = function() {
   if(window.scrollY > window.innerHeight - 10){
     console.log("Scroll Y passed the window inner height")
-    myNavHeader.classList.add('bg-slate-700')
-    myNavHeader.classList.add('opacity-90')
+    myNavHeader.classList.add('bg-teal-900')
     myNavHeader.classList.remove('bg-transparent')
     myNav.classList.remove("mt-5")
   }else{
     console.log("Scroll Y doesn't passed the window inner height")
-    myNavHeader.classList.remove('bg-slate-700')
-    myNavHeader.classList.remove('opacity-90')
+    myNavHeader.classList.remove('bg-teal-900')
     myNavHeader.classList.add('bg-transparent')
     myNav.classList.add("mt-5")
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html", "./js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'af-green': '#26d780',
+      },
+    },
   },
   plugins: [],
 }
